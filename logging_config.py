4d15d0ac5291321ba1b8ab0d6707b5dc012c56dd
Mateusz Sieradzki <35726839+sieradzki@users.noreply.github.com>
2024-12-24 11:13:20 +0100

    colorlog

--- logging_config.py
+++ logging_config.py
@@ -1,13 +1,25 @@
 import logging
 import sys
+import colorlog
 
 
 def configure_logging():
-  """ Configures the root logger. """
+  """ Configures the root logger with colored output. """
+
+  handler = colorlog.StreamHandler()
+  handler.setFormatter(colorlog.ColoredFormatter(
+    "%(log_color)s[%(levelname)s|%(module)s|L%(lineno)d] %(asctime)s: %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S%z",
+    log_colors={
+      'DEBUG': 'cyan',
+      'INFO': 'green',
+      'WARNING': 'yellow',
+      'ERROR': 'red',
+      'CRITICAL': 'red,bg_white',
+    },
+  ))
 
   logging.basicConfig(
     level=logging.DEBUG,
-    format="[%(levelname)s|%(module)s|L%(lineno)d] %(asctime)s: %(message)s",
-    datefmt="%Y-%m-%dT%H:%M:%S%z",
-    handlers=[logging.StreamHandler(sys.stdout)]
+    handlers=[handler]
   )
